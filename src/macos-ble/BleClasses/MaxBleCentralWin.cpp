7bf0b9483df0ed1925ca2e566d858b59416c4e16
Matthew Hamilton <mhamilt7@ed.ac.uk>
2021-07-02 11:59:05 +0100

    add obligatory casting

--- src/macos-ble/BleClasses/MaxBleCentralWin.cpp
+++ src/macos-ble/BleClasses/MaxBleCentralWin.cpp
@@ -3,74 +3,75 @@
 
 MaxBleCentral* newBleCentralC(void)
 {
-    return NULL;
+    return new WinBleCentral;
+}
+
+void bleCentralCRelease(MaxBleCentral* t)
+{
+    delete ((WinBleCentral*)t);
 }
 
 void bleCentralCScan(MaxBleCentral* t)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCStopScan(MaxBleCentral* t)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCConnectToFoundDevice(MaxBleCentral* t, int deviceIndex)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCConnectToDeviceWithUUID(MaxBleCentral* t, const char* uuid)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCConnectToDeviceWithName(MaxBleCentral* t, const char* name)
 {
-
-}
-void bleCentralCRelease(MaxBleCentral* t)
-{
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCSetMaxObjRef(MaxBleCentral* t, MaxExternalObject* maxObjRef)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCGetDeviceList(MaxBleCentral* t)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCClearDiscovered(MaxBleCentral* t)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCGetRssi(MaxBleCentral* t, int deviceIndex)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCSetRSSISensitity(MaxBleCentral* t, int rssiLimit)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCSetReporting(MaxBleCentral* t, bool shouldReport)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCSubscribeToCharacteristic(MaxBleCentral* t, int deviceIndex, const char* suuid, const char* cuuid)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCUnsubscribeToCharacteristic(MaxBleCentral* t, int deviceIndex, const char* suuid, const char* cuuid)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCSetRSSIScanThreshold(MaxBleCentral* t, int rssi)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCSetIgnoreiPhone(MaxBleCentral* t, bool shouldIgnore)
 {
-
+    ((WinBleCentral*)t);
 }
 void bleCentralCScanForServices(MaxBleCentral* t, t_atom* argv, long argc)
 {
-
+    ((WinBleCentral*)t);
 }
